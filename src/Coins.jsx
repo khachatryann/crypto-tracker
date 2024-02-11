a4e255f9ab3a1df384a8096923dd0db4205930ee
khachatryann <khachatryan.aram2006@gmail.com>
2024-02-11 09:02:26 +0400

    Adding some small features...

--- src/Coins.jsx
+++ src/Coins.jsx
@@ -1,7 +1,5 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import info from  "../node_modules/cryptocurrency-icons/32/color/dot.png"
-
 
 
 export const Coins = () => {
@@ -14,13 +12,18 @@ export const Coins = () => {
         .then((res) => {
           setlistOfCoins(res.data.data)
         })
+        .catch(error => {
+            console.error('Error fetching data:', error)
+        })
     }, [])
+    
 
     return(
         <>
-            <div className='search__bar'>
-                <input type="search"  placeholder='Search' onChange={(e) => setSearchCoin(e.target.value)}/>
-            </div>
+            <div className='header'>
+                <h1>Today's Cryptocurrency Prices by Market Cap 💸</h1>
+                <input type="search"  placeholder='Search Cryptocurrency 🔎' onChange={(e) => setSearchCoin(e.target.value)}/>
+            </div>  
             <table>
                 <thead>
                     <tr>
@@ -33,35 +36,45 @@ export const Coins = () => {
                         <th>Circulating Supply</th>
                     </tr>
                 </thead>    
-                {listOfCoins
-                .filter(coin => {
-                    return coin.name.toLowerCase().startsWith(searchCoin.toLocaleLowerCase()) || coin.symbol.toLowerCase().startsWith(searchCoin.toLocaleLowerCase())                  
-                })
-                .map((coin) => {
-                    return (
-                        <tbody key={coin.id}>
-                            <tr>
-                                <td>{coin.rank}</td>
-                                <td>
-                                    <img className='icon' src={"images/icons/" + coin.symbol.toLowerCase() + ".png"} alt='Icon' />
-                                    <p>{coin.name}</p>
-                                    <p>{coin.symbol}</p>    
-                                </td>
-                                <td>{'$' + parseFloat(coin.priceUsd).toFixed(2)}</td>
-                                <td>
-                                    {
-                                        coin.changePercent24Hr > 0 
-                                        ? <p style={{'color' : 'rgb(30, 191, 113)'}}>{'+' + parseFloat(coin.changePercent24Hr).toFixed(2) + '%' }</p>
-                                        : (<p style={{'color' : 'rgb(226, 32, 52)'}}>{parseFloat(coin.changePercent24Hr).toFixed(2) + '%'}</p>)
-                                    }
-                                </td>
-                                <td>{'$' + Math.round(coin.marketCapUsd)}</td>
-                                <td>{'$' + Math.round(coin.volumeUsd24Hr)}</td>
-                                <td>{Math.round(coin.supply) + ' ' + coin.symbol}</td>
-                            </tr>   
-                        </tbody>
-                    )})}
-            </table>    
+                <tbody>
+                    {listOfCoins.length > 0 ? (
+                        listOfCoins
+                            .filter(coin => {  
+                                return coin.name.toLowerCase().includes(searchCoin.toLocaleLowerCase()) || coin.symbol.toLowerCase().includes(searchCoin.toLocaleLowerCase())               
+                            })  
+                            .map((coin) => {
+                                return (
+                                        <tr key={coin.id}>
+                                            <td>{coin.rank}</td>
+                                            <td>
+                                                <div className="name__box">
+                                                    <img className='icon' src={"images/icons/" + coin.symbol.toLowerCase() + ".png"} alt='Icon' />
+                                                    <p className='coin__name'>{coin.name}</p>
+                                                    <p className='coin__symbol'>{coin.symbol}</p>    
+                                                </div>
+                                            </td>
+                                            <td>{'$' + parseFloat(coin.priceUsd).toFixed(2)}</td>
+                                            <td>
+                                                {
+                                                    coin.changePercent24Hr > 0 
+                                                    ? <p style={{'color' : 'rgb(30, 191, 113)'}}>{'+' + parseFloat(coin.changePercent24Hr).toFixed(2) + '%' }</p>
+                                                    : (<p style={{'color' : 'rgb(226, 32, 52)'}}>{parseFloat(coin.changePercent24Hr).toFixed(2) + '%'}</p>)
+                                                }
+                                            </td>
+                                            <td>{'$' + Math.round(coin.marketCapUsd)}</td>
+                                            <td>{'$' + Math.round(coin.volumeUsd24Hr)}</td>
+                                            <td>{Math.round(coin.supply) + ' ' + coin.symbol}</td>
+                                        </tr>   
+                                )
+                    })
+        ) : (
+                <tr>
+                    <td colSpan="7">No coins found.</td>
+                </tr>
+        )
+            }
+        </tbody>
+            </table>   
         </>
     )
 }
